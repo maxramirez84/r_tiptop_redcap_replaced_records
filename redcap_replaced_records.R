d2ddec16b1456a67616b19c09aa2fbf486f9dbcb
maxramirez84 <maxramirez84@gmail.com>
2021-11-03 17:02:20 +0100

    Renamed log table columns.

--- redcap_replaced_records.R
+++ redcap_replaced_records.R
@@ -3,13 +3,14 @@
 # project. There's no API call to retrieve logs)
 kLoggingFileName <- "TIPTOPHHSEndlineMozambique_Logging_2021-11-03_1457.csv"
 logs <- read.csv(kLoggingFileName)
+colnames(logs) <- c("datetime", "username", "action", "data")
 
 # Keep only logs related to record creation through the API
 kRecordCreatedPattern <- "Created Record (API)"
-logs <- logs[which(startsWith(logs$Action, kRecordCreatedPattern)), ]
+logs <- logs[which(startsWith(logs$action, kRecordCreatedPattern)), ]
 
 # Keep only log entries which are duplicated on the Action column, i.e.
 # records created more than once or replaced records
-replaced.cond <- duplicated(logs$Action) | duplicated(logs$Action, fromLast = T)
+replaced.cond <- duplicated(logs$action) | duplicated(logs$action, fromLast = T)
 replaced <- logs[replaced.cond, ]
-replaced <- replaced[order(replaced$Action, replaced$Time...Date), ]
+replaced <- replaced[order(replaced$action, replaced$datetime), ]
